b6051f13d00da2e57f6f239d544b2585f496213f
JJnvn <raptor.jj05@gmail.com>
2024-07-22 23:55:25 +0700

    base component

---
+++ todo-list/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import TodoList from "./TodoList.jsx";
+
+function App() {
+    return (
+        <>
+            <TodoList />
+        </>
+    );
+}
+
+export default App;
