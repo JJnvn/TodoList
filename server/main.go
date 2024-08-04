5c77362c212f52d825caed29af26920ccc5049e4
JJnvn <raptor.jj05@gmail.com>
2024-08-04 13:45:36 +0700

    fix: remove old code

--- server/main.go
+++ server/main.go
@@ -4,12 +4,10 @@ import (
 	"todo-list/handler"
 	"todo-list/repository"
 	"todo-list/service"
-	"todo-list/utils"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/jmoiron/sqlx"
-	"golang.org/x/crypto/bcrypt"
 
 	_ "github.com/go-sql-driver/mysql"
 )
@@ -34,76 +32,9 @@ func main() {
 
 	app.Post("/signup", usersHandler.HandleSignUp)
 	app.Post("/login", usersHandler.HandleLogin)
-	app.Post("/login2", Login)
 	app.Get("/hello", Hello)
 
 	app.Listen(":8000")
-}
-
-// func Signup(c *fiber.Ctx) error {
-
-// 	request := SignupRequest{}
-// 	err := c.BodyParser(&request)
-// 	if err != nil {
-// 		return err
-// 	}
-
-// password, err := bcrypt.GenerateFromPassword([]byte(request.Password), 10)
-// if err != nil {
-// 	return fiber.NewError(fiber.StatusUnprocessableEntity, err.Error())
-// }
-
-// query := "insert users (username, password) values (?, ?)"
-// result, err := db.Exec(query, request.Username, string(password))
-// if err != nil {
-// 	return fiber.NewError(fiber.StatusUnprocessableEntity, err.Error())
-// }
-
-// id, err := result.LastInsertId()
-// if err != nil {
-// 	return fiber.NewError(fiber.StatusUnprocessableEntity, err.Error())
-// }
-
-// response := SignupResponse{
-// 	User: User{
-// 		Id:       int(id),
-// 		Username: request.Username,
-// 		Password: string(password),
-// 	},
-// 	Message: "Sign up success!",
-// }
-
-// 	return c.Status(fiber.StatusCreated).JSON(response)
-// }
-
-func Login(c *fiber.Ctx) error {
-
-	request := LoginRequest{}
-	err := c.BodyParser(&request)
-	if err != nil {
-		return err
-	}
-
-	err = utils.HandleEmptyUserOrPass(request.Username, request.Password)
-	if err != nil {
-		return err
-	}
-
-	user := User{}
-	query := "select id, username, password FROM users WHERE username = ?"
-
-	err = db.Get(&user, query, request.Username)
-
-	if err != nil {
-		return fiber.NewError(fiber.StatusUnprocessableEntity, "user not found")
-	}
-
-	err = bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(request.Password))
-	if err != nil {
-		return fiber.NewError(fiber.StatusUnauthorized, "Invalid username or password")
-	}
-
-	return c.JSON(fiber.Map{"message": "Login successful"})
 }
 
 func Hello(c *fiber.Ctx) error {
